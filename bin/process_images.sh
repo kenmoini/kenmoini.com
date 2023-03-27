11b214676e7f80041341d5d9d998a9ee1dc24efc
Ken Moini <ken@kenmoini.com>
2023-03-26 20:49:35 -0400

    keep the originals around

--- bin/process_images.sh
+++ bin/process_images.sh
@@ -11,8 +11,12 @@ echo -e "\n===== Converting JPEG images...\n"
 find $1 -type f -and \( -iname "*.jpg" -o -iname "*.jpeg" \) -exec bash -c '
 webp_path=$(sed 's/\.[^.]*$/.webp/' <<< "$0");
 if [ ! -f "$webp_path" ]; then
+  echo "Backing up $0 to $0.bak";
+  cp "$0" "$0.bak";
   echo "Converting $0 to $webp_path";
   ./libwebp-1.2.1-linux-x86-64/bin/cwebp -metadata none -quiet -q 75 "$0" -o "$webp_path";
+  echo "Restoring original $0.bak to $0";
+  mv "$0.bak" "$0";
 fi;' {} \;
 
 # converting PNG images
@@ -20,6 +24,10 @@ echo -e "\n===== Converting PNG images...\n"
 find $1 -type f -and -iname "*.png" -exec bash -c '
 webp_path=$(sed 's/\.[^.]*$/.webp/' <<< "$0");
 if [ ! -f "$webp_path" ]; then
+  echo "Backing up $0 to $0.bak";
+  cp "$0" "$0.bak";
   echo "Converting $0 to $webp_path";
   ./libwebp-1.2.1-linux-x86-64/bin/cwebp -metadata none -quiet -q 75 "$0" -o "$webp_path";
+  echo "Restoring original $0.bak to $0";
+  mv "$0.bak" "$0";
 fi;' {} \;
